435fad0c7b7757f5b8a0a2ab6d60777a6a8c6509
hslee0722 <hslee220796@gmail.com>
2026-07-24 15:35:58 +0900

    Update nvic_config.c
    Added comments in NVIC_Config function for clarity.

--- improved/nvic_config.c
+++ improved/nvic_config.c
@@ -1,12 +1,14 @@
 #include "device_driver.h"
 #include "nvic_config.h"
 
+/*
+ * 인터럽트 우선순위 (그룹 3 : preempt 4bit / sub 0bit)
+ */
 void NVIC_Config(void)
 {
     NVIC_SetPriorityGrouping(3);
 
-    NVIC_SetPriority(RTC_Alarm_IRQn, NVIC_EncodePriority(3, 0, 0));
-    NVIC_SetPriority(USART1_IRQn,    NVIC_EncodePriority(3, 1, 0));
-    NVIC_SetPriority(USART2_IRQn,    NVIC_EncodePriority(3, 2, 0));
-    NVIC_SetPriority(EXTI15_10_IRQn, NVIC_EncodePriority(3, 3, 0));
+    NVIC_SetPriority(RTC_Alarm_IRQn, NVIC_EncodePriority(3, 0, 0));  /* 복약 알람 */
+    NVIC_SetPriority(USART1_IRQn,    NVIC_EncodePriority(3, 1, 0));  /* HC-06 앱 명령 */
+    NVIC_SetPriority(USART2_IRQn,    NVIC_EncodePriority(3, 2, 0));  /* PC 디버그 */
 }
