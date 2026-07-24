cb7556683653a8a68f1c251522f822dcb85d10a8
hslee0722 <hslee220796@gmail.com>
2026-07-24 15:34:38 +0900

    Update rtc_calibration.h

--- improved/rtc_calibration.h
+++ improved/rtc_calibration.h
@@ -1,7 +1,11 @@
 #ifndef RTC_CALIBRATION_H
 #define RTC_CALIBRATION_H
 
-void RTC_SmoothCalib_Set(int calp, int calm);
-void RTC_Calibration_From_Daily_Error(float daily_error_sec);
+#ifndef RTC_CALIB_DAILY_ERROR_SEC
+#define RTC_CALIB_DAILY_ERROR_SEC   0.0f
+#endif
+
+int RTC_SmoothCalib_Set(int calp, int calm);
+int RTC_Calibration_From_Daily_Error(float daily_error_sec);
 
 #endif
