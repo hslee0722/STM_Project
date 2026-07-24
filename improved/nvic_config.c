6f9f33a98ed1c6ca704b2b3a357ddbf5f29dce20
hslee0722 <hslee220796@gmail.com>
2026-07-24 15:46:14 +0900

    Update nvic_config.c

--- improved/nvic_config.c
+++ improved/nvic_config.c
@@ -2,7 +2,7 @@
 #include "nvic_config.h"
 
 /*
- * 인터럽트 우선순위 (그룹 3 : preempt 4bit / sub 0bit)
+ * 인터럽트 우선순위 명시 (그룹 3 : preempt 4bit / sub 0bit)
  */
 void NVIC_Config(void)
 {
@@ -11,4 +11,5 @@ void NVIC_Config(void)
     NVIC_SetPriority(RTC_Alarm_IRQn, NVIC_EncodePriority(3, 0, 0));  /* 복약 알람 */
     NVIC_SetPriority(USART1_IRQn,    NVIC_EncodePriority(3, 1, 0));  /* HC-06 앱 명령 */
     NVIC_SetPriority(USART2_IRQn,    NVIC_EncodePriority(3, 2, 0));  /* PC 디버그 */
+    NVIC_SetPriority(EXTI9_5_IRQn,   NVIC_EncodePriority(3, 3, 0));  /* 확인 버튼 */
 }
